ae84e8253333ad2ecb1dd55372bfa00c31342de7
gpromero <gpromero221@gmail.com>
2020-02-24 22:34:36 +0000

    Initial commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:description)
+  end
+
+end
